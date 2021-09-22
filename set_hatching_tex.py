35c9d5ad3f1661544facaa453969f5aa1f03572a
hristo <41540889+arabadzhiyski@users.noreply.github.com>
2021-09-22 19:28:04 +0200

    Initial Commit.

---
+++ set_hatching_tex.py
@@ -0,0 +1,67 @@
+# Sets the 'hatchingTex#' multiparm string fields of PxrStylizedHatching display filter
+# from a sequence of up to 8 *.tex files in Houdini. If the sequence contains less than
+# 8 textures, the beginning and the end will be clamped.
+# It's yours to use for free and I hope you find it useful. Happy rendering!
+
+import hou
+import os
+
+tool_title= "Set Hatching Textures"
+filter_type = "pxrstylizedhatching::3.0"
+tex_isfile = []
+selected = hou.selectedNodes()
+target = None
+
+try:
+    if not selected:
+        raise Exception("Nothing is selected.")
+    else:
+        target = selected[0]
+        
+    if target.type() == hou.nodeType(hou.vopNodeTypeCategory(), filter_type):
+        
+        tex_seq = hou.ui.selectFile(start_directory = "$HIP/tex",
+                                    title = tool_title,
+                                    collapse_sequences = True,
+                                    file_type = hou.fileType.Any,
+                                    pattern = "*.tex",
+                                    default_value = None,
+                                    multiple_select = False,
+                                    image_chooser = False,
+                                    chooser_mode = hou.fileChooserMode.Read)
+    
+        if(tex_seq != ""):
+            for n in range(8):
+                parm_name = "hatchingTex{num}".format(num = n+1)
+                tex_parm = target.parm(parm_name)
+                abs_tex_file_path = hou.text.expandStringAtFrame(tex_seq, n+1)
+                tex_parm.set(hou.text.collapseCommonVars(abs_tex_file_path, vars = ["$HIP"]))
+
+                if os.path.isfile(abs_tex_file_path):
+                    tex_isfile.append(True)
+                else:
+                    tex_isfile.append(False)
+                    
+            if not all(tex_isfile):
+            
+                true_idcs = [i+1 for i, x in enumerate(tex_isfile) if x is True]
+                
+                for idx, val in enumerate(tex_isfile):
+                    if val is False:
+                        if idx+1 < true_idcs[0]:
+                        
+                            first_available = target.parm("hatchingTex{num}".format(num = true_idcs[0])).eval()
+                            target.parm("hatchingTex{num}".format(num = idx+1)).set(hou.text.collapseCommonVars(first_available,vars = ["$HIP"]))
+                            
+                        else:
+                            last_available = target.parm("hatchingTex{num}".format(num = true_idcs[-1])).eval()
+                            target.parm("hatchingTex{num}".format(num = idx+1)).set(hou.text.collapseCommonVars(last_available,vars = ["$HIP"]))
+                                  
+        else:
+            raise Exception("Cancelled")
+    else:
+        raise Exception("Target node must be of type {required_type}, not {selected_type}".format(required_type = filter_type,
+                                                                                                  selected_type = target.type().name()))
+
+except Exception as inst:
+    print(inst.args[0])
