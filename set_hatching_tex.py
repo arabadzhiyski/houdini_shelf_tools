5caab319c08d942d59a05f829ea54399e78060b2
hristo <41540889+arabadzhiyski@users.noreply.github.com>
2021-09-23 11:16:28 +0200

    Update set_hatching_tex.py

--- set_hatching_tex.py
+++ set_hatching_tex.py
@@ -42,24 +42,22 @@ try:
                 else:
                     tex_isfile.append(False)
 
-            valid_tex_index = list(range(1, 8))
-
             if not all(tex_isfile):
                 valid_tex_index = [i+1 for i, x in enumerate(tex_isfile) if x is True]
                 
-            for idx, val in enumerate(tex_isfile):
-                parm_index = idx + 1
-                slot = None
-                if val is False:
-                    if parm_index < valid_tex_index[0]:
-                        slot = valid_tex_index[0]
+                for idx, val in enumerate(tex_isfile):
+                    parm_index = idx + 1
+                    slot = None
+                    if val is False:
+                        if parm_index < valid_tex_index[0]:
+                            slot = valid_tex_index[0]
+                        else:
+                            slot = valid_tex_index[-1]
                     else:
-                        slot = valid_tex_index[-1]
-                else:
-                    slot = parm_index
+                        slot = parm_index
 
-                next_available = target.parm("hatchingTex{num}".format(num = slot)).eval()
-                target.parm("hatchingTex{num}".format(num = idx+1)).set(hou.text.collapseCommonVars(next_available, vars = ["$HIP"]))             
+                    next_available = target.parm("hatchingTex{num}".format(num = slot)).eval()
+                    target.parm("hatchingTex{num}".format(num = idx+1)).set(hou.text.collapseCommonVars(next_available, vars = ["$HIP"]))             
         else:
             raise Exception("Cancelled")
             
